ce8af59d0db96dffa4674507d468df189170fe68
mikeydgithub <ampgzo2@gmail.com>
2022-05-23 21:43:01 -0400

    hero page styling

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Portfolio <br />
+        Mike Diamond
+      </SectionTitle>
+      <SectionText>
+        A motive and aspiring developer looking to expand on my skills and knowledge of learning.
+      </SectionText>
+      <Button onClick={() => window.location = '//https:google.com' }>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
