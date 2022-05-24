8eda61fb981966b049fc193443a1a2de5d6b8007
mikeydgithub <ampgzo2@gmail.com>
2022-05-23 23:18:01 -0400

    complete footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+      <LinkColumn>
+        <LinkTitle>Call</LinkTitle>
+        <LinkItem href="tel: 954-295-9085">954-295-9085</LinkItem>
+      </LinkColumn>
+      <LinkColumn>
+        <LinkTitle>Email</LinkTitle>
+        <LinkItem href="mailto:ampgzo2@gmail.com">ampgzo2@gmail.com</LinkItem>
+      </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+        <SocialIcons href="https://github.com">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://linkedin.com">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://instagram.com">
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
