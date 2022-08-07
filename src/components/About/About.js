b2b045d851079bb81235792f00da3ca3ebf2bc3f
mikeydgithub <ampgzo2@gmail.com>
2022-08-07 19:52:08 -0400

    create commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
     
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
-      <SectionText>Recent University of Central Florida Bootcamp graduate. Looking to utilize and develop my skills of full stack web developer and IT experince.</SectionText>
+      <SectionText>Full-Stack Web developer leveraging a rich background in leadership in healthcare and IT related experiences to build more intuitive user experiences on the web. Recently earned a certificate from the University of Central Florida and developed skills using HTML, CSS, JavaScript, Node, MySQL, MongoDB, GitHub, Heroku, React, and various other languages and libraries.  Possesses strong agility for learning, problem-solving, dedication to teamwork, adapting to challenges, and working on new projects. Uses developer expertise for analysis, design, implementation, testing, deployment, and maintenance of code.  Excited to continue to grow and learn new languages such as Python, Ruby, Angular, C#, and Microsoft .NET framework. Looking to bring keen attention to detail and technical skills to a company to grow and become a valuable asset.</SectionText>
       <div>
       <img src="https://user-images.githubusercontent.com/94988620/172021796-ff385b2a-1191-44d6-9e34-653a2b8e7dd9.png"></img>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
+
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import About from '../components/About/About';
 import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
+
 
 
 const Home = () => {
